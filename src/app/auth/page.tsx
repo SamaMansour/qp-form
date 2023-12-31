c2d28748fb8d7c704c3999a9a5b9d269bf25b8a7
Sama Mansour <samamansour306@gmail.com>
2023-12-31 07:55:25 +0300

    handle form using formik and yup

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,68 +1,68 @@
 // /pages/index.tsx
 "use client"; // This is a client component 👈🏽
 // pages/index.tsx
-// pages/index.tsx
-import { useState } from 'react';
-import { Form, Input, Button, Card, message } from 'antd';
+import { useFormik } from 'formik';
+import { Button, Card, Form, Input, message } from 'antd';
+import * as Yup from 'yup';
 
 export default function Home() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            username: Yup.string().required('Please input your username!'),
+            password: Yup.string().required('Please input your password!'),
+        }),
+        onSubmit: async (values) => {
+            try {
+                const response = await fetch('/api/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(values),
+                });
 
-    const handleSubmit = async (event: React.FormEvent) => {
-        try {
-            const response = await fetch('/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
+                const data = await response.json();
 
-            const data = await response.json();
-
-            if (response.ok) {
-                // Display success message
-                message.success('Login successful');
-                // Additional actions on successful login (e.g., redirect)
-            } else {
-                // Display error message
-                message.error(data.message || 'Invalid username or password');
+                if (response.ok) {
+                    message.success('Login successful');
+                    // Additional actions on successful login (e.g., redirect)
+                } else {
+                    message.error(data.message || 'Invalid username or password');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                message.error('An error occurred, please try again later');
             }
-        } catch (error) {
-            console.error('Error:', error);
-            message.error('An error occurred, please try again later');
-        }
-    };
-
-
+        },
+    });
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f0f2f5' }}>
             <Card title="Login to Your Account" style={{ width: 400, boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)', borderRadius: '10px' }}>
-                <Form
-                    onFinish={handleSubmit}
-                    layout="vertical"
-                >
-                    <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
-                    >
-                        <Input value={username} onChange={(e) => setUsername(e.target.value)} />
+                <Form layout="vertical" onFinish={formik.handleSubmit}>
+                    <Form.Item label="Username" help={formik.touched.username && formik.errors.username}>
+                        <Input 
+                            name="username" 
+                            onChange={formik.handleChange} 
+                            onBlur={formik.handleBlur} 
+                            value={formik.values.username} 
+                        />
                     </Form.Item>
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                    >
-                        <Input.Password value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <Form.Item label="Password" help={formik.touched.password && formik.errors.password}>
+                        <Input.Password 
+                            name="password" 
+                            onChange={formik.handleChange} 
+                            onBlur={formik.handleBlur} 
+                            value={formik.values.password} 
+                        />
                     </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" block>
-                            Login
-                        </Button>
-                    </Form.Item>
+                    <Button type="primary" htmlType="submit" block disabled={formik.isSubmitting}>
+                        Login
+                    </Button>
                 </Form>
             </Card>
         </div>
